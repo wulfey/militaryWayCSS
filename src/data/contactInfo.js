7989c4c8d29d50f1ace9915bec2963470eb53857
Jason Wolfe <jasonwolfe@gmail.com>
2018-12-30 16:39:31 -0800

    jason

--- src/data/contactInfo.js
+++ src/data/contactInfo.js
@@ -1,12 +1,5 @@
-const CONTACT_INFO2 = {
-  name: 'Jason Wolfe',
-  phone: '(310) 562 - 0755',
-  address: '562 Military Way, Palo Alto, CA',
-  email: 'JasonWolfe@gmail.com',
-};
-
 const CONTACT_INFO = {
-  name: 'Patrick ZIMMMs',
+  name: 'Jason Wolfe',
   phone: '(310) 562 - 0755',
   address: '562 Military Way, Palo Alto, CA',
   email: 'JasonWolfe@gmail.com',
